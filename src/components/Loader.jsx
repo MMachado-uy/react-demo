1b08900d77b45a2d2660ea82e51ba8aba66f7ee9
Mauricio Machado <mauricio.machado@modelit.xyz>
2018-01-21 12:14:26 -0300

    Added a loader animation component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Loader extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+			<div className="loader">
+				<div className="bouncer-1"></div>
+				<div className="bouncer-2"></div>
+			</div>
+        );
+    }
+}
+
+export default Loader
